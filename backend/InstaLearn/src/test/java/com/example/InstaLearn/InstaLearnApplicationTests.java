0a82f5ed56e0dc032476faefc7a571c1592889c2
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-03 09:26:49 +0530

    backend

---
+++ backend/InstaLearn/src/test/java/com/example/InstaLearn/InstaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InstaLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
