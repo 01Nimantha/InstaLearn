4a0bd40af454329ff7de1d44db35c3d3a57a9374
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-09 00:26:58 +0530

    create QuestionPaperImpl.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionPaper/impl/QuestionPaperImpl.java
@@ -0,0 +1,8 @@
+package com.example.InstaLearn.questionPaper.impl;
+
+import com.example.InstaLearn.questionPaper.QuestionPaperService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionPaperImpl implements QuestionPaperService {
+}
