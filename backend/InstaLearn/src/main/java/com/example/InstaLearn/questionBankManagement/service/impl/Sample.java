65bedd055ab0266eac64086f4fa437dd8e8c01ec
Dinithi2001 <dinithimadushika2001@gmail.com>
2025-01-08 15:54:58 -0800

    add impl in service package

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionBankManagement/service/impl/Sample.java
@@ -0,0 +1,4 @@
+package com.example.InstaLearn.questionBankManagement.service.impl;
+
+public class Sample {
+}
---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/userManagement/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.example.InstaLearn.userManagement.service;
+
+import com.example.InstaLearn.userManagement.dto.AdminSaveRequestDTO;
+
+public interface AdminService {
+
+    void saveAdmin(AdminSaveRequestDTO adminSaveRequestDTO);
+}
