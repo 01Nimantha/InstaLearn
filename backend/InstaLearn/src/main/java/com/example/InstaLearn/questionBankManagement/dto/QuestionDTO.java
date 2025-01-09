51d1b444534f352b548c8184a3557cf519218125
sudeshtharaka <154073864+sudeshtharaka@users.noreply.github.com>
2025-01-09 22:37:56 +0530

    create post method of QuestionController

--- backend/InstaLearn/src/main/java/com/example/InstaLearn/questionBankManagement/dto/QuestionDTO.java
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionBankManagement/dto/QuestionDTO.java
@@ -1,7 +1,6 @@
 package com.example.InstaLearn.questionBankManagement.dto;
 
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class QuestionsDTO {
+public class QuestionDTO {
     private int questionId;
     private String chapterName;
     private String question;
---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionBankManagement/service/QuestionService.java
@@ -0,0 +1,7 @@
+package com.example.InstaLearn.questionBankManagement.service;
+
+import com.example.InstaLearn.questionBankManagement.dto.QuestionDTO;
+
+public interface QuestionService {
+    public String saveQuestion(QuestionDTO questionDTO);
+}
