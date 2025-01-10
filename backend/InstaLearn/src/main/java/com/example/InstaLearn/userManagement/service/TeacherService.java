bfcbc481a2702198c565c7ebcba8d0a5997a1932
malee <maleeshanimanthi2000@gmail.com>
2025-01-10 11:46:01 +0530

    create basic crud operations for teacher

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/userManagement/service/TeacherService.java
@@ -0,0 +1,12 @@
+package com.example.InstaLearn.userManagement.service;
+
+import com.example.InstaLearn.userManagement.dto.TeacherSaveRequestDTO;
+import com.example.InstaLearn.userManagement.dto.TeacherUpdateRequestDTO;
+
+public interface TeacherService {
+    String saveTeacher(TeacherSaveRequestDTO teacherSaveRequestDTO) ;
+
+    String updateTeacher(TeacherUpdateRequestDTO teacherUpdateRequestDTO);
+
+    String deleteTeacher(int teacherId);
+}
