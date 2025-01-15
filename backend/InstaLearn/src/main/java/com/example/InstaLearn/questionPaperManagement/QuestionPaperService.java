032eb0f55b66a730bedfbd2fc94182bd61c7ae1d
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-15 09:44:35 +0530

    Update QuestionPaerService.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionPaperManagement/QuestionPaperService.java
@@ -0,0 +1,20 @@
+package com.example.InstaLearn.questionPaperManagement;
+
+import com.example.InstaLearn.questionPaperManagement.dto.QuestionPaperDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionPaperService {
+    List<QuestionPaper> findAllQuestionPaper();
+
+    QuestionPaper findQuestionPaperByIdOrThrow(int id);
+
+    boolean saveQuestionPaper(QuestionPaperDto questionPaper);
+
+    boolean updateQuestionPaper(int id, QuestionPaper questionPaper);
+
+    boolean deleteQuestionPaperById(int id);
+
+
+}
