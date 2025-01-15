2283fa34ec33c342c8421fa15095c9e0eff8e115
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-15 10:33:55 +0530

    create StudentAnswerRepository.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/studentAnswerManagement/StudentAnswerRepository.java
@@ -0,0 +1,6 @@
+package com.example.InstaLearn.studentAnswerManagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentAnswerRepository extends JpaRepository<StudentAnswer,Integer> {
+}
