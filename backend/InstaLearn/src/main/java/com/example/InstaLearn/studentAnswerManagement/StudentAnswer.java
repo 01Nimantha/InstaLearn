81a5044fe460d1f64ca6f9f21e2d1f2802eaff9c
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-17 09:27:49 +0530

    Update StudentAnswer.java

--- backend/InstaLearn/src/main/java/com/example/InstaLearn/studentAnswerManagement/StudentAnswer.java
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/studentAnswerManagement/StudentAnswer.java
@@ -16,7 +16,7 @@ public class StudentAnswer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int st_id;
+    private String st_id;
     private String st_answer;
     private int qp_id;
     private int q_id;
