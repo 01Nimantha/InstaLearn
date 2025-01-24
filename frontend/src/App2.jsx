4e61d7dcc4afc34faf249b4dbc9d77e52a7dfd3b
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-24 11:00:46 +0530

    Update App2.jsx

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from "./components/Sidebar"
+import Button from "./components/Button"
 
 const App2 = () => {
   return (
     <div>
+      <Button name={"Nimantha"} action={()=>{console.log("Nimantha Click")}}/>
       <Sidebar/>
+     
     </div>
   );
 };
