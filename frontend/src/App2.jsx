ba056da9e30d2d68b8828a04e4ad5e8970ee396d
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-01-24 10:02:46 +0530

    Create App2.jsx

---
+++ frontend/src/App2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from "./components/Sidebar"
+
+const App2 = () => {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  );
+};
+
+export default App2;
