b3d61f92161db5bbdbc158c9ce5ebb915ab0d682
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-10 16:41:25 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import  { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App2 from './App2'
 // import App from './App.jsx'
 
+const router =createBrowserRouter([{path:"/",element:<App2/>}])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     {/* <App /> */}
-    <App2/>
+    {/* <App2/> */}
   </StrictMode>,
 )
