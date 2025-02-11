99d1dfc5eecdf5ae82f898daabc10644d16e8868
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-11 20:49:25 +0530

    Update QuizeCard.jsx

--- frontend/src/components/QuizCard.jsx
+++ frontend/src/components/QuizCard.jsx
@@ -3,15 +3,15 @@ import Button from "./Button";
 const QuizCard =({QuizCardDuration,QuizCardDate,QuizCardPrecentage})=>{
   return <div className="card" style={{marginLeft:"2%",marginTop:"2%", minWidth:"47%", maxWidth:"47%"}}>
     <div style={{display:"flex"}}>
-      <div>
+      <div style={{width:"30%"}}>
         <img src={QuizImg}  alt="..." style={{minHeight:"100%"}}/> 
       </div>
-      <div style={{marginTop:"5%"}}>
+      <div style={{marginTop:"1%",marginBottom:"1%",marginLeft:"2%"}}>
         <div>{QuizCardDuration}</div>
         <div>{QuizCardDate}</div>
         <div>{QuizCardPrecentage}</div>
       </div>
-      <div style={{marginTop:"10%",marginLeft:"2%",marginRight:"2%"}}>
+      <div style={{marginTop:"6%",marginLeft:"14%",marginRight:"2%"}}>
         <Button name={"Check"} backgroundColor={"#78D9C6"}fontColor={"#FFFFFF"} cornerRadius={true} 
         />
       </div>
