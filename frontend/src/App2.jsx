1bd9cbff693fb4653f6ee5e5cc522aa4d2028ae6
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-12 16:04:40 +0530

    Update App2.jsx

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from "./components/Sidebar"
 // import Button from "./components/Button"
-import Card from "./components/Card"
 import styles from "./App2.module.css";
-import OnlineQuiz from "./assets/OnlineQuiz.svg"
-import HomeWork from "./assets/HomeWork.svg"
 import StudentImg from "./assets/StudentImg.svg"
-import ImgCard from "./components/ImgCard";
-import EventCard from "./components/EventCard";
 import QuizCard from "./components/QuizCard";
 import PaperHearderCard from "./components/PaperHearderCard";
 import PaperCard from "./components/PaperCard";
@@ -18,24 +13,16 @@ import { MdOutlinePayment } from "react-icons/md";
 import { HiCalendarDateRange } from "react-icons/hi2";
 import { IoIosSettings } from "react-icons/io";
 import { HiMiniDocumentCurrencyDollar } from "react-icons/hi2";
+import { Outlet } from "react-router-dom";
+
 
 const App2 = () => {
   return (
     <div>
       <div className={styles.container}>
-      <Sidebar ImgURL={StudentImg} Name={"Alia Bhatt"} Id={"SC/2021/12405"} Logout={()=>{console.log("Click Logout Button")}} Tab1={"Home"} Tab1Icon={FaHome} Tab1functions={()=>{console.log("Click Tab 1")}} Tab2={"Payment"} Tab2Icon={MdOutlinePayment} Tab2functions={()=>{console.log("Click Tab 2")}} Tab3={"Timetable"} Tab3Icon={HiCalendarDateRange} Tab3functions={()=>{console.log("Click Tab 3")}} Tab4={"Setting"} Tab4Icon={IoIosSettings} Tab4functions={()=>{console.log("Click Tab 4")}} Tab5={"Payment History"} Tab5Icon={HiMiniDocumentCurrencyDollar} Tab5functions={()=>{console.log("Click Tab 5")}}/>
+      <Sidebar BackgroundColor={"#13A68A"} ImgURL={StudentImg} Name={"Alia Bhatt"} Id={"SC/2021/12405"} Logout={()=>{console.log("Click Logout Button")}} Tab1={"Home"} Tab1Icon={FaHome} Tab1functions={"/"} Tab2={"Payment"} Tab2Icon={MdOutlinePayment} Tab2functions={"/payment"} Tab3={"Timetable"} Tab3Icon={HiCalendarDateRange} Tab3functions={"/timetable"} Tab4={"Setting"} Tab4Icon={IoIosSettings} Tab4functions={"/setting"} Tab5={"Payment History"} Tab5Icon={HiMiniDocumentCurrencyDollar} Tab5functions={"/payment-history"} AddNewTab={true} Tab6={"New Tab"} Tab6Icon={FaHome} Tab6functions={"/new-tab"}/>
       <div>
-        <div className={styles.containerbody}>
-          <ImgCard ImgCardName={"Alia Bhatt"} ImgCardImg={StudentImg} ImgCardID={"SC/2021/12405"}/>
-          <EventCard />
-        </div>
-        <div>
-          <div style={{marginLeft:"2%"}}>Activities</div>
-          <div className={styles.containerbody}>
-            <Card CardImg={OnlineQuiz} CardTitle={"Online Quiz"} CardBody={"Start : 2025/3/24 End : 2025/3/25 Duration: 8.00am -10.00am"} CardButtonName={"Start quiz now"} CardButtonBackgroundColor={"#78D9C6"} CardButtonFontColor={"#FFFFFF"} CardButtonCornerRadius={true} />
-            <Card CardImg={HomeWork} CardTitle={"Home Work"} CardBody={"Great achievements start with small, consistent steps your homework is one of them."} CardButtonName={"View"} CardButtonBackgroundColor={"#78D9C6"} CardButtonFontColor={"#FFFFFF"} CardButtonCornerRadius={true} />
-          </div>
-        </div>
+        <Outlet/>
         {/* remove below */}
         <div style={{display:"flex"}}>
           <QuizCard QuizCardDuration={"08.00am -10.00am"} QuizCardDate={"2025-03-24"} QuizCardPrecentage={"100%"}/><QuizCard QuizCardDuration={"08.00am -10.00am"} QuizCardDate={"2025-03-24"} QuizCardPrecentage={"100%"}/>
