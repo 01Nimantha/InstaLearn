5781730c2fa8979422a86222c247f582794f55fb
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-18 00:41:35 +0530

    Create userSlice.js

---
+++ frontend/src/store/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice =createSlice({
+  name:"user",
+  initialState:{userArr:[
+    {
+      Id: "SC12405",
+      Name: "Alia Bhatt",
+      Email: "abc@gmail.com",
+      Number: "076 789 2017",
+      Address: "123 Main Street, Colombo, Western Province, 10000",
+      ParentName: "Soni Razdan",
+      ParentNumber: "076 700 2087"
+    }
+  ]},
+  reducers:{
+    updateUser:(state,action)=>{
+      state.userArr=action.payload;
+    }
+  }
+});
+
+export const userAction = userSlice.actions
+export default userSlice;
