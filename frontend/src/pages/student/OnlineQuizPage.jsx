88b29cae7f6928f30374f642806c96ced322c6e9
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-18 00:06:04 +0530

    Update OnlineQuizPage.jsx

--- frontend/src/pages/student/OnlineQuizPage.jsx
+++ frontend/src/pages/student/OnlineQuizPage.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import QuizCard from "../../components/QuizCard";
 
 const OnlineQuizPage=()=>{
 
+  const quiz = useSelector((store)=>store.quizreducer);
   return  <div>
             <div className="fs-1" style={{marginBottom:"2%"}}> <center>Online Quiz Progress</center></div>
             <div className="container text-center">
                 <div className="row row-cols-2">
-                  {[...Array(5)].map((_, index) => (<div key={index} className="col">
-                    <QuizCard QuizCardDuration={"08.00am -10.00am"} QuizCardDate={"2025-03-24"} QuizCardPrecentage={"100%"}/>
+                  {quiz.quizArr.map((item, index) => (<div key={index} className="col">
+                    <QuizCard QuizCardDuration={item.Duration} QuizCardDate={item.Date} QuizCardPrecentage={item.Mark}/>
                     </div>))}
                 </div>
               </div>
