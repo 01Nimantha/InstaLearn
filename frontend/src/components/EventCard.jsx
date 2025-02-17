27fe57131731e950501fa128a635994d9e95a971
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-18 00:05:32 +0530

    Update EventCard.jsx

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,21 +1,40 @@
+import { useSelector } from "react-redux";
 import Button from "./Button";
 import { FaBell } from "react-icons/fa";
+import { useState } from "react";
 const EventCard = ()=>{
+  const event = useSelector((store)=>store.eventreducer);
+  console.log("Event ",event.eventarr.length);
+  const [num,setNum] = useState(0);
+  const next = () => {
+    if (event.eventarr.length > num + 1) {
+      setNum((prevNum) => prevNum + 1); 
+    }
+  };
+
+  const previous = () => {
+    if (num > 0) {
+      setNum((prevNum) => prevNum - 1); 
+    }
+  };
+
   return <div className="card" style={{margin:"2% 2% 2% 0%",maxWidth:"70%", minWidth:"70%"}}>
     <div  style={{backgroundColor:"#13A68A",borderRadius:"5px 5px 0px 0px",marginBottom:"1%",padding:"1%", display:"flex"}}>
-      <div style={{marginLeft:"1%"}}>Upcoming events</div>
-      <div style={{marginLeft:"77%"}}><FaBell size={24} color="red"/></div>
+      <div style={{marginLeft:"1%"}}>New Events <span class="badge bg-danger">{event.eventarr.length}</span></div>
+      <div style={{marginLeft:"77%",display:"flex"}}>
+      <FaBell size={25} color="red"/><span className="badge" style={{position:"absolute",right:"3.3%"}}>{num+1}</span></div>
+      
     </div>
     <div className="card" style={{maxWidth:"98%",margin:"0% 2% 2% 2%", backgroundColor:"#78D9C6",minHeight:"80%"}}>
-      <p style={{color:"#ffffff", marginLeft:"2%"}}>Kabaddi event </p>
-      <div className="card" style={{backgroundColor:"#78D9C6",marginLeft:"2%",marginRight:"2%",maxHeight:"60%",maxWidth:"96%",minWidth:"96%",overflowY: "auto",textAlign:"center"}}> 🔥 Get ready for an action-packed Kabaddi Tournament filled with strength, strategy, and spirit! Witness thrilling matches as top teams battle it out for victory! Witness thrilling matches as top teams battle itWitness thrilling matches as top teams battle it out for victory! Witness thrilling matches as top teams battle it out for victory! out for victory! Witness thrilling matches as top teams battle it out for victory!Witness thrilling matches as top teams battle it out for victory! Witness thrilling matches as top teams battle it out for victory!
+      <p style={{color:"#ffffff", marginLeft:"2%"}}>{event.eventarr[num].eventName}</p>
+      <div className="card" style={{backgroundColor:"#78D9C6",marginLeft:"2%",marginRight:"2%",maxHeight:"60%",maxWidth:"96%",minWidth:"96%",overflowY: "auto",textAlign:"center"}}> {event.eventarr[num].eventBody}
       </div>
       <div style={{margin:"1%",alignSelf:"end",display:"flex",marginRight:"3%"}}>
         <div style={{marginRight:"3%"}}>
-          <Button name={"Previous"} action={()=>{console.log("Previous Button Click")}} backgroundColor={"#ffffff"} fontColor={"#000000"}cornerRadius={true}/>
+          <Button name={"Previous"} action={previous} backgroundColor={"#ffffff"} fontColor={"#000000"}cornerRadius={true}/>
         </div>
         <div>
-          <Button name={"Next"} action={()=>{console.log("Next Button Click")}} backgroundColor={"#ffffff"} fontColor={"#000000"} cornerRadius={true}/></div>
+          <Button name={"Next"} action={next} backgroundColor={"#ffffff"} fontColor={"#000000"} cornerRadius={true}/></div>
       </div>
     </div>
   </div>;
