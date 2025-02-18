e9f9583b4070818053180eab0eef681a0896a39d
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-18 19:29:41 +0530

    Update App2.jsx

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -10,13 +10,15 @@ import { HiCalendarDateRange } from "react-icons/hi2";
 import { IoIosSettings } from "react-icons/io";
 import { HiMiniDocumentCurrencyDollar } from "react-icons/hi2";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
 const App2 = () => {
+  const student = useSelector((store)=>store.studentreducer.studentArr[0]);
   return (
     <div>
       <div className={styles.container}>
-        <Sidebar BackgroundColor={"#13A68A"} ImgURL={StudentImg} Name={"Alia Bhatt"} Id={"SC/2021/12405"} Logout={()=>{console.log("Click Logout Button")}} Tab1={"Home"} Tab1Icon={FaHome} Tab1functions={"/"} Tab2={"Payment"} Tab2Icon={MdOutlinePayment} Tab2functions={"/payment"} Tab3={"Timetable"} Tab3Icon={HiCalendarDateRange} Tab3functions={"/timetable"} Tab4={"Setting"} Tab4Icon={IoIosSettings} Tab4functions={"/setting"} Tab5={"Payment History"} Tab5Icon={HiMiniDocumentCurrencyDollar} Tab5functions={"/payment-history"} AddNewTab={true} Tab6={"New Tab"} Tab6Icon={FaHome} Tab6functions={"/new-tab"}/>
+        <Sidebar BackgroundColor={"#13A68A"} ImgURL={StudentImg} Name={student.Name} Id={student.Id} Logout={()=>{console.log("Click Logout Button")}} Tab1={"Home"} Tab1Icon={FaHome} Tab1functions={"/"} Tab2={"Payment"} Tab2Icon={MdOutlinePayment} Tab2functions={"/payment"} Tab3={"Timetable"} Tab3Icon={HiCalendarDateRange} Tab3functions={"/timetable"} Tab4={"Setting"} Tab4Icon={IoIosSettings} Tab4functions={"/setting"} Tab5={"Payment History"} Tab5Icon={HiMiniDocumentCurrencyDollar} Tab5functions={"/payment-history"} AddNewTab={true} Tab6={"New Tab"} Tab6Icon={FaHome} Tab6functions={"/new-tab"}/>
         <div>
           <Outlet/>
         </div>
