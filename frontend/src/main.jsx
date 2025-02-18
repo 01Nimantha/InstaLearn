2a77ddee34542ba6575066d2fae8638557494f51
sudeshtharaka <154073864+sudeshtharaka@users.noreply.github.com>
2025-02-18 09:55:35 +0530

    resolve rouer dom error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -34,7 +34,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={mystore}>
     <RouterProvider router={router} />
     </Provider>
-    {/* <App /> */}
+    <App />
     {/* <App2/> */}
     {/* <App3/> */}
     
