8f4c23c7e8086468c13c67736727455fb7b23ce1
200055501221 <maleeshanimanthi2000@gmail.com>
2025-02-18 17:53:31 +0530

    Started to create modal pop up in teachers dashboard

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -8,12 +8,12 @@ const Modal = ({isVisible,onClose,children}) => {
     }
 
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center'id='wrapper' onClick={handleClose}>Modal
+    <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center 'id='wrapper' onClick={handleClose} > Modal
     <div>
         <div className='bg-white p-5 rounded-lg'>
             <h1 className='text-2xl font-bold'>{children}</h1>
-            <p>Modal Content</p>
-            <button className='bg-blue-500 text-white px-3 py-1 rounded-lg'>Close</button>
+            <p>Modal</p>
+            <button className='bg-blue-500 text-white px-3 py-1 rounded-lg'>Submit</button>
         </div>
     </div>
     </div>
