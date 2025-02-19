8171c19f4ab5ac7dd5bf5523721ce500706b47c0
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-19 22:21:39 +0530

    Update UserHomePage.jsx

--- frontend/src/pages/student/UserHomePage.jsx
+++ frontend/src/pages/student/UserHomePage.jsx
@@ -6,7 +6,7 @@ import HomeWork from "../../assets/HomeWork.svg"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import PerformanceOverview from "../../components/PerformanceOverview";
+import OnlineQuizProgress from "../../components/OnlineQuizProgress";
 const UserHomePage=()=>{
   const student = useSelector((store)=>store.studentreducer.studentArr[0]);
   const imageURL = useSelector((store)=>store.imagereducer.imagePath);
@@ -19,12 +19,12 @@ const UserHomePage=()=>{
   <div>
     <div style={{marginLeft:"2%"}}>Activities</div>
     <div style={{display: "flex"}}>
-      <Card CardImg={OnlineQuiz} CardTitle={"Online Quiz"} CardBody={"Start : 2025/3/24 End : 2025/3/25 Duration: 8.00am -10.00am"} CardButtonName={"Start quiz now"} CardButtonBackgroundColor={"#78D9C6"} CardButtonFontColor={"#FFFFFF"} CardButtonCornerRadius={true} CardButtonAction={()=>{navigate("online-quiz")}} />
+      <Card CardImg={OnlineQuiz} CardTitle={"Online Quiz"} CardBody={"Start : 2025/3/24 End : 2025/3/25 Duration: 8.00am -10.00am"} CardButtonName={"Start quiz now"} CardButtonBackgroundColor={"#78D9C6"} CardButtonFontColor={"#FFFFFF"} CardButtonCornerRadius={true} CardButtonAction={()=>{navigate("online-qpaper")}} />
       <Card CardImg={HomeWork} CardTitle={"Home Work"} CardBody={"Great achievements start with small, consistent steps your homework is one of them."} CardButtonName={"View"} CardButtonBackgroundColor={"#78D9C6"} CardButtonFontColor={"#FFFFFF"} CardButtonCornerRadius={true} />
     </div>
   </div>
   <div>
-    <PerformanceOverview/>
+    <OnlineQuizProgress/>
   </div>
 </div>;
 }
