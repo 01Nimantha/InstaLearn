b8ff9035dfcb771ceaac50e401c95806dd0d2148
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-19 08:32:31 +0530

    Update UserSettingPage.jsx

--- frontend/src/pages/student/UserSettingPage.jsx
+++ frontend/src/pages/student/UserSettingPage.jsx
@@ -1,11 +1,10 @@
 import UploadPhotoCard from "../../components/UploadPhotoCard";
-import StudentImg from "../../assets/StudentImg.svg"
 import UploadStudentDetailsCard from "../../components/UploadStudentDetailsCard";
 // import UploadParentDetailsCard from "../../components/UploadParentDetailsCard";
 
 const UserSettingPage=()=>{
   return <div>
-  <UploadPhotoCard ImgURL={StudentImg}/>
+  <UploadPhotoCard />
   <UploadStudentDetailsCard/>
   {/* <UploadParentDetailsCard/> */}
 </div>;
