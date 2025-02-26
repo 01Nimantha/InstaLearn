9e7c90a1fbd776fe562ed8fee14513aba72d74de
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-26 19:49:20 +0530

    Update StudentAnswerDto.java

--- backend/InstaLearn/src/main/java/com/example/InstaLearn/studentAnswerManagement/dto/StudentAnswerDto.java
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/studentAnswerManagement/dto/StudentAnswerDto.java
@@ -1,5 +1,6 @@
 package com.example.InstaLearn.studentAnswerManagement.dto;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,12 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class StudentAnswerDto {
+    @Column(name = "st_id")
     private String st_id;
     private String st_answer;
+    @Column(name = "qp_id")
     private int qp_id;
     private int q_id;
+    private boolean mark;
+    private boolean disable;
 }
