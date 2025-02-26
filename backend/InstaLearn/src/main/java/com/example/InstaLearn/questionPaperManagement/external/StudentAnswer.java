ffb675eaf8fcb129ee72530ff83cec583637c0f3
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-26 19:46:12 +0530

    Update StudentAnswer.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionPaperManagement/external/StudentAnswer.java
@@ -0,0 +1,18 @@
+package com.example.InstaLearn.questionPaperManagement.external;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StudentAnswer {
+    private String st_id;
+    private String st_answer;
+    private int qp_id;
+    private int q_id;
+    private boolean mark;
+    private boolean disable;
+}
