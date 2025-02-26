ce2b22213729d0e7d9d59ac2575fe0258501a320
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-26 11:18:11 +0530

    Update QuestionPaperPage.jsx

--- frontend/src/pages/student/QuestionPaperPage.jsx
+++ frontend/src/pages/student/QuestionPaperPage.jsx
@@ -1,11 +1,13 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import PaperCard from "../../components/PaperCard";
 import PaperHearderCard from "../../components/PaperHearderCard";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
+import { quistionAction } from "../../store/quistionSlice";
 
 const QuestionPaperPage=()=>{
   const quistions = useSelector((store)=>store.quistionreducer.quistionArr);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   
   return <div>
@@ -16,7 +18,9 @@ const QuestionPaperPage=()=>{
       {quistions.map((item, index) => (<PaperCard key={index} QuestionID={item.id} Question={item.question} Answer1={item.options[0]} Answer2={item.options[1]} Answer3={item.options[2]} Answer4={item.options[3]} CorrectAnswer={item.correctAnswer} Disable={item.disable}  StudentAnswer={item.studentAnswer}/>))}
 
       <div style={{marginLeft:"67.8vw",marginBottom:"2vw"}}>
-        <Button name={"Submit"} action={()=>{navigate("/student-dashboard") }} backgroundColor={"#78D9C6"} fontColor={"#ffffff"} cornerRadius={false} />
+        <Button name={"Submit"} action={()=>{
+          dispatch(quistionAction.makeDisable());
+          navigate("/student-dashboard"); }} backgroundColor={"#78D9C6"} fontColor={"#ffffff"} cornerRadius={false} />
       </div>
     </div>
   </div>;
