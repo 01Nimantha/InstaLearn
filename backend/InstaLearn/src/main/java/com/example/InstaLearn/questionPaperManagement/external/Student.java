6e14bb1685fb7aac093ff984b6970aea727c6919
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-02-26 19:45:43 +0530

    Update Student.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionPaperManagement/external/Student.java
@@ -0,0 +1,12 @@
+package com.example.InstaLearn.questionPaperManagement.external;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+    private String studentID;
+}
