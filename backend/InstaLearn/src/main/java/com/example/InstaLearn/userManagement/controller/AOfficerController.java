a0a58e04f637827a62516790deab5ea661dcd2aa
Dinithi Madushika <dinithimadushika2001@gmail.com>
2025-03-24 13:12:39 -0700

    change getAllAttendanceOfficers method for pagination

--- backend/InstaLearn/src/main/java/com/example/InstaLearn/userManagement/controller/AOfficerController.java
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/userManagement/controller/AOfficerController.java
@@ -11,6 +11,10 @@ import com.example.InstaLearn.userManagement.service.AOfficerService;
 import com.example.InstaLearn.userManagement.service.AdminService;
 import com.example.InstaLearn.userManagement.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,25 +32,25 @@ public class AOfficerController {
     public ResponseEntity<StandardResponse> saveAttendanceOfficer(@RequestBody AOfficerSaveRequestDTO aOfficerSaveRequestDTO) {
         String message = aOfficerService.saveAttendanceOfficer(aOfficerSaveRequestDTO);
         return new ResponseEntity<StandardResponse>(
-                new StandardResponse(201,"success",message),
+                new StandardResponse(201, "success", message),
                 HttpStatus.CREATED
         );
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<StandardResponse> updateAttendanceOfficer(@PathVariable(value="id") String attendanceOfficerId,@RequestBody AOfficerUpdateRequestDTO aOfficerUpdateRequestDTO) {
-        String message = aOfficerService.updateAttendanceOfficer(attendanceOfficerId,aOfficerUpdateRequestDTO);
+    public ResponseEntity<StandardResponse> updateAttendanceOfficer(@PathVariable(value = "id") String attendanceOfficerId, @RequestBody AOfficerUpdateRequestDTO aOfficerUpdateRequestDTO) {
+        String message = aOfficerService.updateAttendanceOfficer(attendanceOfficerId, aOfficerUpdateRequestDTO);
         return new ResponseEntity<StandardResponse>(
-                new StandardResponse(200,"success",message),
+                new StandardResponse(200, "success", message),
                 HttpStatus.OK
         );
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<StandardResponse> deleteAttendanceOfficer(@PathVariable(value="id") String attendanceOfficerId) {
+    public ResponseEntity<StandardResponse> deleteAttendanceOfficer(@PathVariable(value = "id") String attendanceOfficerId) {
         String message = aOfficerService.deleteAttendanceOfficer(attendanceOfficerId);
         return new ResponseEntity<StandardResponse>(
-                new StandardResponse(200,"success",message),
+                new StandardResponse(200, "success", message),
                 HttpStatus.OK
         );
     }
@@ -54,6 +58,26 @@ public class AOfficerController {
 //    public ResponseEntity<List<AttendanceOfficer>> getAllAttandanceOfficers(){
 //        return new ResponseEntity<>(aOfficerService.getAllAttandanceOfficers(), HttpStatus.FOUND);
 //    }
+
+    @GetMapping("/get-all-aOfficers")
+    public ResponseEntity<Page<AttendanceOfficer>> getAllAttandanceOfficers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "attendanceOfficerName,asc") String sort
+    ) {
+        String[] sortParams = sort.split(",");
+        String sortBy = sortParams[0];
+        String direction = sortParams[1];
+
+        Sort sortObj = Sort.by(Sort.Direction.fromString(direction), sortBy);
+        Pageable pageable = PageRequest.of(page, size, sortObj);
+
+        Page<AttendanceOfficer> attendanceOfficers = aOfficerService.getAllAttandanceOfficers(pageable);
+
+        return new ResponseEntity<>(attendanceOfficers, HttpStatus.OK);
+    }
+
+
     @GetMapping("/get-aOfficer-by/{id}")
     public AttendanceOfficer getAttendanceOfficerById(@PathVariable(value="id") String attendanceOfficerId) {
         return aOfficerService.getAttendanceOfficerById(attendanceOfficerId);
