b6c172524b073f6f99be956c341961f1113c7f5d
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-04-04 11:35:47 +0530

    Update UserHomePage.jsx

--- frontend/src/pages/student/UserHomePage.jsx
+++ frontend/src/pages/student/UserHomePage.jsx
@@ -5,12 +5,39 @@ import OnlineQuiz from "../../assets/OnlineQuiz.svg"
 import HomeWork from "../../assets/HomeWork.svg"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import OnlineQuizProgress from "../../components/OnlineQuizProgress";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { quistionAction } from "../../store/quistionSlice";
+
 const UserHomePage=()=>{
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  
+
   const student = useSelector((store)=>store.studentreducer.studentArr[0]);
   const imageURL = useSelector((store)=>store.imagereducer.imagePath);
   const navigate =useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8085/QuestionPaper/GetNewfullPaper/ST_2025_10001")
+      .then((response) => {
+        setLoading(false);
+        dispatch(quistionAction.addQuistion(response.data));
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return <div>
   <div style={{display: "flex"}}>
     <ImgCard ImgCardName={student.Name} ImgCardImg={imageURL} ImgCardID={student.Id}/>
