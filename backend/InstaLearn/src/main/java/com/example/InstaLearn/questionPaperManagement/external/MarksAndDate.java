b3e660ffcad1f084adf8a11d591febf1e2297839
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-04-05 10:44:35 +0530

    Create MarksAndDate.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionPaperManagement/external/MarksAndDate.java
@@ -0,0 +1,13 @@
+package com.example.InstaLearn.questionPaperManagement.external;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MarksAndDate {
+    private int mark;
+    private String date;
+}
