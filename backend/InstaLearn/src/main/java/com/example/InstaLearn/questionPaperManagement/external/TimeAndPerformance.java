af45aeae30ef9d6a88118a85eb5d77191df87215
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-04-06 17:53:17 +0530

    Create TimeAndPerformance.java

---
+++ backend/InstaLearn/src/main/java/com/example/InstaLearn/questionPaperManagement/external/TimeAndPerformance.java
@@ -0,0 +1,13 @@
+package com.example.InstaLearn.questionPaperManagement.external;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TimeAndPerformance {
+    private String time;
+    private int performance;
+}
