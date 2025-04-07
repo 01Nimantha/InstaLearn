732ff61b2fae7a8209e276c8eca51d448401a9f7
Dinithi Madushika <dinithimadushika2001@gmail.com>
2025-04-07 10:29:40 -0700

    backend changes

--- backend/InstaLearn/src/main/java/com/example/InstaLearn/TimeTableManagement/ScheduleManagementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.InstaLearn.scheduleManagement2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleManagementRepository extends JpaRepository<Event,Integer> {
-
-}
