07df1fa95edc7cea7e45aea101e59d2ef22d633e
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-04-08 09:08:40 +0530

    Update App2.jsx

--- frontend/src/App2.jsx
+++ frontend/src/App2.jsx
@@ -17,17 +17,20 @@ const App2 = () => {
   const student = useSelector((store)=>store.studentreducer.studentArr[0]);
   const imageURL = useSelector((store)=>store.imagereducer.imagePath);
   return (
-    <div>
-      <div className={styles.container}>
-        <Sidebar BackgroundColor={"#13A68A"} ImgURL={imageURL} Name={student.Name} Id={student.Id} Logout={()=>{console.log("Click Logout Button")}} Tab1={"Home"} Tab1Icon={FaHome} Tab1functions={"/student-dashboard/"+id} Tab2={"Payment"} Tab2Icon={MdOutlinePayment} Tab2functions={"payment"} Tab3={"Timetable"} Tab3Icon={HiCalendarDateRange} Tab3functions={"timetable"} Tab4={"Setting"} Tab4Icon={IoIosSettings} Tab4functions={"settings"} Tab5={"Payment History"} Tab5Icon={HiMiniDocumentCurrencyDollar} Tab5functions={"payment-history"} AddNewTab={false}/>
-        <div>
-          <Outlet/>
-        </div>
-        {/* <Button name={"Next"} action={()=>{console.log("Nimantha Click")}} backgroundColor={"#78D9C6"} fontColor={"#FFFFFF"} cornerRadius={false}/> */}
+    <div className='flex min-h-screen'>
+      <div className="fixed top-0 left-0 h-full"
+        style={{ width: '280px' }}>  
+       <Sidebar BackgroundColor={"#13A68A"} ImgURL={imageURL} Name={student.Name} Id={student.Id} Logout={()=>{console.log("Click Logout Button")}} Tab1={"Home"} Tab1Icon={FaHome} Tab1functions={"/student-dashboard/"+id} Tab2={"Payment"} Tab2Icon={MdOutlinePayment} Tab2functions={"payment"} Tab3={"Timetable"} Tab3Icon={HiCalendarDateRange} Tab3functions={"timetable"} Tab4={"Setting"} Tab4Icon={IoIosSettings} Tab4functions={"settings"} Tab5={"Payment History"} Tab5Icon={HiMiniDocumentCurrencyDollar} Tab5functions={"payment-history"} AddNewTab={false}/>
       </div>
-     
+      
+       {/* Main content area that takes up the remaining space */}
+       <div className="flex-1 bg-gray-100" style={{ marginLeft: '280px' }}>
+       <Outlet/>
+      </div>
     </div>
   );
 };
 
 export default App2;
+
+
